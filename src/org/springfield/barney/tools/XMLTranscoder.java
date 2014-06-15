9abe2fa97e3efcd772809f7ddce6c9abdd80b2d0
scb2scb2 <daniel@xs4all.nl>
2014-06-15 21:33:31 +0300

    added license

--- src/org/springfield/barney/tools/XMLTranscoder.java
+++ src/org/springfield/barney/tools/XMLTranscoder.java
@@ -1,3 +1,23 @@
+/* 
+* XMLTranscoder.java
+* 
+* Copyright (c) 2012 Noterik B.V.
+* 
+* This file is part of barney, related to the Noterik Springfield project.
+*
+* Barney is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Barney is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Lou.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.springfield.barney.tools;
 
 import java.io.IOException;
